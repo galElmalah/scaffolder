ad35ed29f90e972631931889918a33f9d51cd392
Gal Elmalah <gale@wix.com>
2020-06-04 11:38:12 +0300

    update error message

--- Errors/NoScaffolderFolder.js
+++ Errors/NoScaffolderFolder.js
@@ -6,7 +6,7 @@ class NoScaffolderFolder extends Error {
   }
 
   getDisplayErrorMessage() {
-    const githubLink = "https://github.com/galElmalah/ctf";
+    const githubLink = "https://github.com/galElmalah/scaffolder";
     const message = `${error(
       "Error there is no scaffolder folder."
     )}\nPlease create a scaffolder folder.\nFor more info checkout: ${path(
