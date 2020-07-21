d230d03f7a2cde86e0b790ac6cd7b3ff4f810006
Gal Elmalah <gale@wix.com>
2020-07-22 02:22:00 +0300

    seperated the default transformers to a different folder

--- src/createTemplateStructure/applyTransformers.js
+++ src/createTemplateStructure/applyTransformers.js
@@ -1,15 +1,5 @@
 const { MissingTransformerImplementation } = require("../../Errors");
-
-const defaultTransformers = {
-  toLowerCase: (value) => value.toLowerCase(),
-  toUpperCase: (value) => value.toLowerCase(),
-  capitalize: (value) => `${value[0].toUpperCase()}${value.substring(1)}`,
-  toCamelCase: (value) => value.replace(/([-_]\w)/g, (g) => g[1].toUpperCase()),
-  camelCaseToSnakeCase: (value) =>
-    value.replace(/[A-Z]/g, (m) => `_${m.toLowerCase()}`),
-  camelCaseToKabebCase: (value) =>
-    value.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`),
-};
+const { defaultTransformers } = require("./defaultTransformers");
 
 const applyTransformers = (initialValue, transformersMap, transformersKeys) => {
   return transformersKeys
---
+++ src/createTemplateStructure/defaultTransformers.js
@@ -0,0 +1,10 @@
+module.exports.defaultTransformers = {
+  toLowerCase: (value) => value.toLowerCase(),
+  toUpperCase: (value) => value.toLowerCase(),
+  capitalize: (value) => `${value[0].toUpperCase()}${value.substring(1)}`,
+  toCamelCase: (value) => value.replace(/([-_]\w)/g, (g) => g[1].toUpperCase()),
+  camelCaseToSnakeCase: (value) =>
+    value.replace(/[A-Z]/g, (m) => `_${m.toLowerCase()}`),
+  camelCaseToKabebCase: (value) =>
+    value.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`),
+};
