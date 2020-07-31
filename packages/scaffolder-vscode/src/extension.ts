fe6a5761eaff2ecfeeb4b386de14555f89dfdc3d
Gal Elmalah <gale@wix.com>
2020-07-31 19:47:32 +0300

    propmpting the user for key value pairs

--- packages/scaffolder-vscode/src/extension.ts
+++ packages/scaffolder-vscode/src/extension.ts
@@ -1,8 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-
-function execuateTerminalCommand(command: string) {
+import * as scaffolder from "scaffolder-cli";
+//Create output channel
+let scaffolderOut = vscode.window.createOutputChannel("Scaffolder");
+scaffolderOut.appendLine("alshdkjaslhdkjash");
+function executeTerminalCommand(command: string) {
   if (vscode.window.activeTerminal?.name === "scaffolder") {
     vscode.window.activeTerminal.sendText(command);
     return;
@@ -13,14 +16,73 @@ function execuateTerminalCommand(command: string) {
 }
 function activateScaffolder(path: string) {
   const ctfCreateTemplateCommand = `scaff i --entry-point ${path}`;
-  execuateTerminalCommand(ctfCreateTemplateCommand);
+  executeTerminalCommand(ctfCreateTemplateCommand);
 }
+
+const getDefaultQuestion = (key: string) =>
+  `Enter a value for the following key "${key}"`;
+
+const getQuestion = (paramsOptions: { [k: string]: any }, key: string) => {
+  return (
+    (paramsOptions[key] && paramsOptions[key].question) ||
+    getDefaultQuestion(key)
+  );
+};
 
 export function activate(context: vscode.ExtensionContext) {
   const whenClickingOnFolderCommand = vscode.commands.registerCommand(
     "scaffolder-vscode.createTemplateInFolder",
-    (uri: vscode.Uri) => {
-      activateScaffolder(uri.fsPath);
+    async (uri: vscode.Uri) => {
+      try {
+        const availableTemplateCommands = await scaffolder.commandsBuilder(
+          uri.fsPath
+        );
+        const { label: chosenTemplate } =
+          (await vscode.window.showQuickPick(
+            Object.keys(availableTemplateCommands).map((template) => ({
+              label: template,
+              detail: `Template path: ${availableTemplateCommands[template]}`,
+            })),
+            {
+              placeHolder: "Choose a template",
+              ignoreFocusOut: true,
+            }
+          )) || {};
+        scaffolderOut.appendLine(JSON.stringify({ chosenTemplate }, null, 2));
+        const { config, currentCommandTemplate } = scaffolder.templateReader(
+          availableTemplateCommands
+        )(chosenTemplate);
+        const templateKeys = scaffolder.extractAllKeysFromTemplate(
+          currentCommandTemplate
+        );
+        scaffolderOut.appendLine(
+          JSON.stringify({ chosenTemplate, config, templateKeys }, null, 2)
+        );
+
+        const paramsValues = {};
+        // get values for the keys
+        for (let param of templateKeys) {
+          const cleanKey = scaffolder.extractKey(param);
+          const paramValue = await vscode.window.showInputBox({
+            prompt: getQuestion(config.parametersOptions, cleanKey),
+            placeHolder: `Enter value (${templateKeys.indexOf(param) + 1}/${
+              templateKeys.length
+            })`,
+          });
+          // @ts-ignore
+          paramsValues[scaffolder.extractKey(param)] = paramValue;
+        }
+        scaffolderOut.appendLine(JSON.stringify(paramsValues, null, 2));
+        vscode.window.showInformationMessage(
+          "Scaffolder: successfully created your template"
+        );
+      } catch (e) {
+        scaffolderOut.appendLine(e);
+      }
+
+      // const values = await scaffolder.commandsBuilder(uri.fsPath);
+
+      // activateScaffolder(uri.fsPath);
     }
   );
 
@@ -30,6 +92,12 @@ export function activate(context: vscode.ExtensionContext) {
       let directoryPath = uri.fsPath.split("/");
       directoryPath.pop();
       (directoryPath as any) = directoryPath.join("/");
+
+      // scaffolder.commandsBuilder(directoryPath).then((r) => {
+      //   scaffolderOut.appendLine(JSON.stringify(r));
+      //   scaffolderOut.appendLine("JSON.stringify(r)");
+      // });
+
       activateScaffolder(directoryPath as any);
     }
   );
@@ -37,7 +105,7 @@ export function activate(context: vscode.ExtensionContext) {
   const listTemplates = vscode.commands.registerCommand(
     "scaffolder-vscode.listTemplates",
     (uri: vscode.Uri) => {
-      execuateTerminalCommand(`scaff ls --entry-point ${uri.fsPath}`);
+      executeTerminalCommand(`scaff ls --entry-point ${uri.fsPath}`);
     }
   );
 
