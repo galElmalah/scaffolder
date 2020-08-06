6ff7593c7957f57b87fd5f4f5c21de7a2b7e0024
Gal Elmalah <gale@wix.com>
2020-08-06 21:32:12 +0300

    add support for validation functions in parameters input

--- packages/scaffolder-vscode/src/getParamsValuesFromUser.ts
+++ packages/scaffolder-vscode/src/getParamsValuesFromUser.ts
@@ -1,5 +1,21 @@
-import { getQuestionMessage, extractKey } from "scaffolder-cli";
+import {
+  getQuestionMessage,
+  extractKey,
+  getValidationFunction,
+} from "scaffolder-cli";
 import * as vscode from "vscode";
+
+const vscodeValidationFunctionAdapter = (parametersOptions: {}, key:string) => {
+  const validationFn = getValidationFunction(parametersOptions, key);
+  if (validationFn) {
+    return (value: string) => {
+      const result = validationFn(value);
+      if (typeof result === "string") {
+        return result;
+      }
+    };
+  }
+};
 
 export const getParamsValuesFromUser = async (
   templateKeys: string[],
@@ -13,6 +29,10 @@ export const getParamsValuesFromUser = async (
       placeHolder: `Enter value (${templateKeys.indexOf(param) + 1}/${
         templateKeys.length
       })`,
+      validateInput: vscodeValidationFunctionAdapter(
+        config.parametersOptions,
+        cleanKey
+      ),
     });
     // @ts-ignore
     paramsValues[extractKey(param)] = paramValue;
