e598a8908142763676bc7a51238d6e637610967b
Gal Elmalah <gale@wix.com>
2020-08-13 22:40:10 +0300

    formmating

--- src/commandsBuilder/index.js
+++ src/commandsBuilder/index.js
@@ -1,37 +1,37 @@
-const fs = require("fs");
-const path = require("path");
-const { NoScaffolderFolder } = require("../../Errors");
-const { isFolder } = require("../filesUtils");
+const fs = require('fs');
+const path = require('path');
+const { NoScaffolderFolder } = require('../../Errors');
+const { isFolder } = require('../filesUtils');
 
-const TEMPLATE_FOLDER_NAME = "scaffolder";
+const TEMPLATE_FOLDER_NAME = 'scaffolder';
 
-const endOfPath = (path) => path === "/" || path === "" || path === "./";
+const endOfPath = (path) => path === '/' || path === '' || path === './';
 
 const templatePathsFinder = (currentPath) => {
-  const pathsQueue = [];
-  const findTemplate = (currentPath) => {
-    if (endOfPath(currentPath) && pathsQueue.length === 0) {
-      throw new NoScaffolderFolder();
-    }
-    if (endOfPath(currentPath)) {
-      return pathsQueue;
-    }
-    const currentDir = fs.readdirSync(currentPath);
+	const pathsQueue = [];
+	const findTemplate = (currentPath) => {
+		if (endOfPath(currentPath) && pathsQueue.length === 0) {
+			throw new NoScaffolderFolder();
+		}
+		if (endOfPath(currentPath)) {
+			return pathsQueue;
+		}
+		const currentDir = fs.readdirSync(currentPath);
 
-    const isScaffolderFolderInThisLevel = currentDir.find(
-      (f) => f === TEMPLATE_FOLDER_NAME
-    );
+		const isScaffolderFolderInThisLevel = currentDir.find(
+			(f) => f === TEMPLATE_FOLDER_NAME
+		);
 
-    if (
-      isScaffolderFolderInThisLevel &&
+		if (
+			isScaffolderFolderInThisLevel &&
       isFolder(currentPath, TEMPLATE_FOLDER_NAME)
-    ) {
-      pathsQueue.push(path.join(currentPath, TEMPLATE_FOLDER_NAME));
-    }
+		) {
+			pathsQueue.push(path.join(currentPath, TEMPLATE_FOLDER_NAME));
+		}
 
-    return findTemplate(path.join(currentPath, "../"));
-  };
-  return findTemplate(currentPath);
+		return findTemplate(path.join(currentPath, '../'));
+	};
+	return findTemplate(currentPath);
 };
 
 /**
@@ -41,21 +41,21 @@ const templatePathsFinder = (currentPath) => {
  */
 const commandsBuilder = (currentPath) => {
 
-  const scaffolderPaths = templatePathsFinder(currentPath);
-  let allCommands = {};
-  for (const scaffolderPath of scaffolderPaths) {
-    const commands = fs.readdirSync(scaffolderPath);
+	const scaffolderPaths = templatePathsFinder(currentPath);
+	let allCommands = {};
+	for (const scaffolderPath of scaffolderPaths) {
+		const commands = fs.readdirSync(scaffolderPath);
 
-    let commandsToPath = commands
-      .filter((p) => p[0] !== ".")
-      .filter((p) => isFolder(scaffolderPath, p))
-      .reduce(
-        (accm, cmd) => ({ ...accm, [cmd]: path.join(scaffolderPath, cmd) }),
-        {}
-      );
-    allCommands = { ...commandsToPath, ...allCommands };
-  }
-  return allCommands;
+		let commandsToPath = commands
+			.filter((p) => p[0] !== '.')
+			.filter((p) => isFolder(scaffolderPath, p))
+			.reduce(
+				(accm, cmd) => ({ ...accm, [cmd]: path.join(scaffolderPath, cmd) }),
+				{}
+			);
+		allCommands = { ...commandsToPath, ...allCommands };
+	}
+	return allCommands;
 };
 
 module.exports = { commandsBuilder, templatePathsFinder };
